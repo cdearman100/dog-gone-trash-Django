589a5c63b3315188b144d24a571b5c9ee8686696
CJ <cdearm2@uic.edu>
2025-01-14 17:06:58 -0600

    Updated settings.py

--- dog_gone_trash/settings.py
+++ dog_gone_trash/settings.py
@@ -37,6 +37,19 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://jolly-horse-0f0796.netlify.app",
+    # or "https://*.netlify.app" if needed
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
